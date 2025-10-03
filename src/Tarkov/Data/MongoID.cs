ef3a63e6f91c16b8b66f25dcb57a7447099c017b
Lone <234549977+lone7254@users.noreply.github.com>
2025-10-03 09:54:15 -0500

    f

--- src/Tarkov/Data/MongoID.cs
+++ src/Tarkov/Data/MongoID.cs
@@ -19,6 +19,7 @@
         /// <param name="cb"></param>
         /// <param name="useCache"></param>
         /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public string ReadString(int cb = 128, bool useCache = true)
         {
             return Memory.ReadUnicodeString(_stringId, cb, useCache);
