0938a5e0e791d146426a02caaf3a15bca54398a3
Lone <234549977+lone7254@users.noreply.github.com>
2025-11-22 01:28:53 -0600

    Revert mutex check wasn't working for hung process during auto-updater :( [no ci]

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -101,17 +101,7 @@ namespace LoneEftDmaRadar
                 VelopackApp.Build().Run();
                 _mutex = new Mutex(true, MUTEX_ID, out bool singleton);
                 if (!singleton)
-                {
-                    _mutex.Dispose();
-                    var thisProc = Process.GetCurrentProcess();
-                    foreach (var proc in
-                        Process.GetProcessesByName(thisProc.ProcessName)
-                        .Where(p => p.Id != thisProc.Id))
-                    {
-                        proc.Kill(); // Kill any zombies
-                    }
-                    Environment.FailFast("Another instance is already running. Please restart the application.");
-                }
+                    throw new InvalidOperationException("The application is already running.");
                 Config = EftDmaConfig.Load();
                 ServiceProvider = BuildServiceProvider();
                 HttpClientFactory = ServiceProvider.GetRequiredService<IHttpClientFactory>();
