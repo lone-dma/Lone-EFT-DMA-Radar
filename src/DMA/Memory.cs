320b1d8ea006757d73f920e7e2b0e29525ac2bec
Lone <234549977+lone7254@users.noreply.github.com>
2025-11-29 12:02:32 -0600

    Remove memory macros and update VmmSharpEx version
    Removed unused memory utility macros from `Memory.cs`, including `PAGE_ALIGN`, `BYTE_OFFSET`, `ADDRESS_AND_SIZE_TO_SPAN_PAGES`, `AlignLength`, and `AlignAddress`. These methods were previously used for memory alignment and page calculations but are no longer required.
    
    Updated the `VmmSharpEx` package in `Lone-EFT-DMA-Radar.csproj` from version `3.110.7` to `3.110.12` to leverage improvements and fixes in the newer version.

--- src/DMA/Memory.cs
+++ src/DMA/Memory.cs
@@ -630,53 +630,5 @@ namespace LoneEftDmaRadar.DMA
         }
 
         #endregion
-
-        #region Memory Macros
-
-        /// <summary>
-        /// The PAGE_ALIGN macro returns a page-aligned virtual address for a given virtual address.
-        /// https://learn.microsoft.com/windows-hardware/drivers/ddi/wdm/nf-wdm-page_align
-        /// </summary>
-        /// <param name="va">Virtual address.</param>
-        /// <returns>Page-aligned virtual address.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong PAGE_ALIGN(ulong va) => va & ~(0x1000ul - 1);
-
-        /// <summary>
-        /// The BYTE_OFFSET macro takes a virtual address and returns the byte offset of that address within the page.
-        /// https://learn.microsoft.com/windows-hardware/drivers/ddi/wdm/nf-wdm-byte_offset
-        /// </summary>
-        /// <param name="va">virtual address.</param>
-        /// <returns>Offset portion of the virtual address within the page.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint BYTE_OFFSET(ulong va) => (uint)(va & (0x1000ul - 1));
-
-        /// <summary>
-        /// The ADDRESS_AND_SIZE_TO_SPAN_PAGES macro returns the number of pages that a virtual range spans.
-        /// The virtual range is defined by a virtual address and the size in bytes of a transfer request.
-        /// https://learn.microsoft.com/windows-hardware/drivers/ddi/wdm/nf-wdm-address_and_size_to_span_pages
-        /// </summary>
-        /// <param name="va">Virtual address that is the base of the range.</param>
-        /// <param name="size">Specifies the size in bytes.</param>
-        /// <returns>Returns the number of pages spanned by the virtual range starting at Va.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong ADDRESS_AND_SIZE_TO_SPAN_PAGES(ulong va, ulong size) =>
-            (BYTE_OFFSET(va) + size + (0x1000ul - 1)) >> 12;
-
-        /// <summary>
-        /// Returns a length aligned to 8 bytes.
-        /// Always rounds up.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint AlignLength(uint length) => (length + 7) & ~7u;
-
-        /// <summary>
-        /// Returns an address aligned to 8 bytes.
-        /// Always the next aligned address.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong AlignAddress(ulong address) => (address + 7) & ~7ul;
-
-        #endregion
     }
 }
