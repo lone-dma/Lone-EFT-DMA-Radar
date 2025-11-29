e0f2b9d7db4d7515a0cbe4f774ca0c6d38546f7d
Lone <234549977+lone7254@users.noreply.github.com>
2025-11-28 23:09:38 -0600

    Remove AddRVA methods and update VmmSharpEx package
    Removed two overloaded AddRVA extension methods from the
    MemoryExtensions class, which calculated new addresses
    using instruction size and relative virtual addresses.
    The class is now empty.
    
    Updated the VmmSharpEx package reference in the project
    file from version 3.110.1 to 3.110.7, reflecting a minor
    version upgrade.
    [no ci]

--- src/DMA/MemoryExtensions.cs
+++ src/DMA/MemoryExtensions.cs
@@ -30,32 +30,6 @@ namespace LoneEftDmaRadar.DMA
 {
     internal static class MemoryExtensions
     {
-        /// <summary>
-        /// Calculates a new address by adding the instruction size and a relative virtual address (RVA) to the current address.
-        /// </summary>
-        /// <param name="address">Current virtual address.</param>
-        /// <param name="instructionSize">Assembly instruction length in bytes.</param>
-        /// <param name="rva">Relative virtual address (RVA).</param>
-        /// <returns>New address calculated from the RVA.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong AddRVA(this ulong address, uint instructionSize, int rva)
-        {
-            // signed rva can be negative, so we need to use long for the calculation
-            long result = (long)address + instructionSize + rva;
-            return (ulong)result;
-        }
-
-        /// <summary>
-        /// Calculates a new address by adding the instruction size and a relative virtual address (RVA) to the current address.
-        /// </summary>
-        /// <param name="address">Current virtual address.</param>
-        /// <param name="instructionSize">Assembly instruction length in bytes.</param>
-        /// <param name="rva">Relative virtual address (RVA).</param>
-        /// <returns>New address calculated from the RVA.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ulong AddRVA(this ulong address, uint instructionSize, uint rva)
-        {
-            return address + instructionSize + rva;
-        }
+        // empty... for now
     }
 }
